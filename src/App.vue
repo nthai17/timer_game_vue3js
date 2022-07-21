7149b85a18561605a057a489cbf630abaef937ea
thaind-2785 <nguyen.duy.thai@sun-asterisk.com>
2022-07-21 20:06:20 +0700

    timer_game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Timer Project</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block_cpnVue v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <Results_cpnVue v-if="showResult" :score="score"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block_cpnVue from './components/Block_cpn.vue'
+import Results_cpnVue from './components/Results_cpn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block_cpnVue,
+    Results_cpnVue
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -22,5 +48,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results_cpn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Reaction time: {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'score'
+  ],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) this.rank = "World class"
+    else if (this.score < 400) this.rank = "Medium"
+    else this.rank = "Chicken boy"
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 20px;
+    font-weight: 700;
+    color: #0faf87;
+  }
+</style>
